112e6ed971508945f10257a46f90da9ba84302e5
Jayson Fong <jaysonfong@gatech.edu>
2022-12-10 22:26:57 -0500

    Added to Home Page

--- client/src/Pages/Home_Page.jsx
+++ client/src/Pages/Home_Page.jsx
@@ -9,6 +9,15 @@ const Home_Page = () => {
 
     function Items() {
         const items = [
+            {"path": "/add_drone", "text": "Add Drone"},
+            {"path": "/add_employee", "text": "Add Employee"},
+            {"path": "/add_ingredient", "text": "Add Ingredient"},
+            {"path": "/add_location", "text": "Add Location"},
+            {"path": "/add_owner", "text": "Add Owner"},
+            {"path": "/add_pilot_role", "text": "Add Pilot Role"},
+            {"path": "/add_restaurant", "text": "Add Restaurant"},
+            {"path": "/add_service", "text": "Add Delivery Service"},
+            {"path": "/add_worker_role", "text": "Add Worker Role"},
             {"path": "/start_funding", "text": "Start Funding"},
             {"path": "/add_owner", "text": "Add Owner"}
         ];
