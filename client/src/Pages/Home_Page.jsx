5703c5d55ffefc0a96194630aa2a89b6c64696c7
Jayson Fong <jaysonfong@gatech.edu>
2022-12-10 20:30:19 -0500

    More Buttons

--- client/src/Pages/Home_Page.jsx
+++ client/src/Pages/Home_Page.jsx
@@ -1,11 +1,31 @@
 const Home_Page = () => {
+    function Item(props) {
+        return <div>
+            <a href={props.path}>
+                <button>{props.text}</button>
+            </a>
+        </div>
+    }
+
+    function Items() {
+        const items = [
+            {"path": "/start_funding", "text": "Start Funding"},
+            {"path": "/add_owner", "text": "Add Owner"}
+        ];
+
+        const itemObjects = [];
+        for (let i = 0; i < items.length; i++) {
+            itemObjects.push(<Item path={items[i]["path"]} text={items[i]["text"]} />)
+        }
+
+        return itemObjects;
+    }
+
     return (
         <div className="App">
             <h1>Restaurant Manager</h1>
             <h2>Select Operation</h2>
-            <a href="/add_pilot_role">
-                <button>Add Pilot Role</button>
-            </a>
+            <Items />
         </div>
     )
 }
